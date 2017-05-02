808f8f331f50a4525ff6de559b6f046d5b47d7e5
dlazarocrn <dlazaro.crn@artsgrafiques.org>
2017-05-02 19:51:09 +0200

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -5,3 +5,15 @@ function desplegar(){
 function tancar(){
 	document.getElementById('burguer').style.left="-75%";
 }
+
+function visible(){
+	document.getElementById('pruebaloca').style.display="inline";
+	document.getElementById('bgviscat').style.display="none";
+	document.getElementById('bginviscat').style.display="inline";
+}
+
+function invisible(){
+	document.getElementById('pruebaloca').style.display="none";
+	document.getElementById('bgviscat').style.display="inline";
+	document.getElementById('bginviscat').style.display="none";
+}
