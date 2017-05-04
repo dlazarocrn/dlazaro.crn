8ed0bdb1dd56f0dd3467c6fb32157e8a784c6683
dlazarocrn <dlazaro.crn@artsgrafiques.org>
2017-05-04 17:30:09 +0200

    Add files via upload

---
+++ js/perfil.js
@@ -0,0 +1,125 @@
+function visible01(){
+	document.getElementById('clipcolor01').style.display="inline";
+	document.getElementById('clipcolor02').style.display="none";
+	document.getElementById('clipcolor03').style.display="none";
+	document.getElementById('clipcolor04').style.display="none";
+	document.getElementById('clipcolor05').style.display="none";
+	document.getElementById('clipgris01').style.display="none";
+	document.getElementById('clipgris02').style.display="inline";
+	document.getElementById('clipgris03').style.display="inline";
+	document.getElementById('clipgris04').style.display="inline";
+	document.getElementById('clipgris05').style.display="inline";
+}
+function visible02(){
+	document.getElementById('clipcolor01').style.display="inline";
+	document.getElementById('clipcolor02').style.display="inline";
+	document.getElementById('clipcolor03').style.display="none";
+	document.getElementById('clipcolor04').style.display="none";
+	document.getElementById('clipcolor05').style.display="none";
+	document.getElementById('clipgris01').style.display="none";
+	document.getElementById('clipgris02').style.display="none";
+	document.getElementById('clipgris03').style.display="inline";
+	document.getElementById('clipgris04').style.display="inline";
+	document.getElementById('clipgris05').style.display="inline";
+}
+function visible03(){
+	document.getElementById('clipcolor01').style.display="inline";
+	document.getElementById('clipcolor02').style.display="inline";
+	document.getElementById('clipcolor03').style.display="inline";
+	document.getElementById('clipcolor04').style.display="none";
+	document.getElementById('clipcolor05').style.display="none";
+	document.getElementById('clipgris01').style.display="none";
+	document.getElementById('clipgris02').style.display="none";
+	document.getElementById('clipgris03').style.display="none";
+	document.getElementById('clipgris04').style.display="inline";
+	document.getElementById('clipgris05').style.display="inline";
+}
+function visible04(){
+	document.getElementById('clipcolor01').style.display="inline";
+	document.getElementById('clipcolor02').style.display="inline";
+	document.getElementById('clipcolor03').style.display="inline";
+	document.getElementById('clipcolor04').style.display="inline";
+	document.getElementById('clipcolor05').style.display="none";
+	document.getElementById('clipgris01').style.display="none";
+	document.getElementById('clipgris02').style.display="none";
+	document.getElementById('clipgris03').style.display="none";
+	document.getElementById('clipgris04').style.display="none";
+	document.getElementById('clipgris05').style.display="inline";
+}
+function visible05(){
+	document.getElementById('clipcolor01').style.display="inline";
+	document.getElementById('clipcolor02').style.display="inline";
+	document.getElementById('clipcolor03').style.display="inline";
+	document.getElementById('clipcolor04').style.display="inline";
+	document.getElementById('clipcolor05').style.display="inline";
+	document.getElementById('clipgris01').style.display="none";
+	document.getElementById('clipgris02').style.display="none";
+	document.getElementById('clipgris03').style.display="none";
+	document.getElementById('clipgris04').style.display="none";
+	document.getElementById('clipgris05').style.display="none";
+}
+
+function invisible01(){
+	document.getElementById('clipcolor01').style.display="inline";
+	document.getElementById('clipcolor02').style.display="none";
+	document.getElementById('clipcolor03').style.display="none";
+	document.getElementById('clipcolor04').style.display="none";
+	document.getElementById('clipcolor05').style.display="none";
+	document.getElementById('clipgris01').style.display="none";
+	document.getElementById('clipgris02').style.display="inline";
+	document.getElementById('clipgris03').style.display="inline";
+	document.getElementById('clipgris04').style.display="inline";
+	document.getElementById('clipgris05').style.display="inline";
+}
+
+function invisible02(){
+	document.getElementById('clipcolor01').style.display="inline";
+	document.getElementById('clipcolor02').style.display="none";
+	document.getElementById('clipcolor03').style.display="none";
+	document.getElementById('clipcolor04').style.display="none";
+	document.getElementById('clipcolor05').style.display="none";
+	document.getElementById('clipgris01').style.display="none";
+	document.getElementById('clipgris02').style.display="inline";
+	document.getElementById('clipgris03').style.display="inline";
+	document.getElementById('clipgris04').style.display="inline";
+	document.getElementById('clipgris05').style.display="inline";
+}
+
+function invisible03(){
+	document.getElementById('clipcolor01').style.display="inline";
+	document.getElementById('clipcolor02').style.display="inline";
+	document.getElementById('clipcolor03').style.display="none";
+	document.getElementById('clipcolor04').style.display="none";
+	document.getElementById('clipcolor05').style.display="none";
+	document.getElementById('clipgris01').style.display="none";
+	document.getElementById('clipgris02').style.display="none";
+	document.getElementById('clipgris03').style.display="inline";
+	document.getElementById('clipgris04').style.display="inline";
+	document.getElementById('clipgris05').style.display="inline";
+}
+
+function invisible04(){
+	document.getElementById('clipcolor01').style.display="inline";
+	document.getElementById('clipcolor02').style.display="inline";
+	document.getElementById('clipcolor03').style.display="inline";
+	document.getElementById('clipcolor04').style.display="inline";
+	document.getElementById('clipcolor05').style.display="none";
+	document.getElementById('clipgris01').style.display="none";
+	document.getElementById('clipgris02').style.display="none";
+	document.getElementById('clipgris03').style.display="none";
+	document.getElementById('clipgris04').style.display="none";
+	document.getElementById('clipgris05').style.display="inline";
+}
+
+function invisible05(){
+	document.getElementById('clipcolor01').style.display="inline";
+	document.getElementById('clipcolor02').style.display="inline";
+	document.getElementById('clipcolor03').style.display="inline";
+	document.getElementById('clipcolor04').style.display="inline";
+	document.getElementById('clipcolor05').style.display="inline";
+	document.getElementById('clipgris01').style.display="none";
+	document.getElementById('clipgris02').style.display="none";
+	document.getElementById('clipgris03').style.display="none";
+	document.getElementById('clipgris04').style.display="none";
+	document.getElementById('clipgris05').style.display="none";
+}
